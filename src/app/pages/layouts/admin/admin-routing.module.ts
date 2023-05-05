a9cd9656898307831af5e8adaac580af51176ca4
Olatunji Ajanaku <olatunjiajanaku7@gmail.com>
2023-05-05 14:08:21 +0100

    remove guard

--- src/app/pages/layouts/admin/admin-routing.module.ts
+++ src/app/pages/layouts/admin/admin-routing.module.ts
@@ -21,32 +21,32 @@ const routes: Routes = [
       },
       {
         path: 'transactions',
-        canActivate: [KycCompleteGuard],
+        // canActivate: [KycCompleteGuard],
         loadChildren: () => import('../../modules/transactions/transaction.module').then(m => m.TransactionModule)
       },
       {
         path: 'refund',
-        canActivate: [KycCompleteGuard],
+        // canActivate: [KycCompleteGuard],
         loadChildren: () => import('../../modules/refund/refund.module').then(m => m.RefundModule)
       },
       {
         path: 'chargeback',
-        canActivate: [KycCompleteGuard],
+        // canActivate: [KycCompleteGuard],
         loadChildren: () => import('../../modules/chargeback/chargeback.module').then(m => m.ChargenModule)
       },
       {
         path: 'settlement',
-        canActivate: [KycCompleteGuard],
+        // canActivate: [KycCompleteGuard],
         loadChildren: () => import('../../modules/settlement/settlement.module').then(m => m.SettlementModule)
       },
       {
         path: 'terminal',
-        canActivate: [KycCompleteGuard],
+        // canActivate: [KycCompleteGuard],
         loadChildren: () => import('../../modules/terminal/terminal.module').then(m => m.TerminalModule)
       },
       {
         path: 'settings',
-        canActivate: [KycCompleteGuard],
+        // canActivate: [KycCompleteGuard],
         loadChildren: () => import('../../modules/settings/settings.module').then(m => m.SettingsModule)
       },
     ]
